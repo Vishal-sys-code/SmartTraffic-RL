95c14f185738573ec6ea423533660d646e3f28a4
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-08-05 19:13:44 +0530

    Update __init__.py

--- smart_traffic_env/__init__.py
+++ smart_traffic_env/__init__.py
@@ -1,7 +1,10 @@
-from .env import UrbanTrafficEnv
 from gymnasium.envs.registration import register
 
+# Expose the environment class
+from .env import UrbanTrafficEnv
+
+# Register the environment
 register(
-    id='UrbanTraffic-v0',
+    id='SmartTraffic-v0',
     entry_point='smart_traffic_env.env:UrbanTrafficEnv',
 )
